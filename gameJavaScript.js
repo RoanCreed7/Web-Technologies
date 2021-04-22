82c1e1701bfb5d453a79536ff6d098fcedd322c9
RoanCreed7 <40430682@live.napier.ac.uk>
2021-04-22 14:28:01 +0100

    update/formatting and comments

--- gameJavaScript.js
+++ gameJavaScript.js
@@ -3,20 +3,17 @@ const storyElement = document.getElementById('story');
 
 var game = false;
 var storyReturn;
-
 var index = 1;
-
 var time;
 var t;
 var difficulty;
 
-function gameStart(){
+function gameStart() {
     let state = {}
     showStory(1);
 }
 
-function continueStory(){
-    console.log("Continue story")
+function continueStory() {
     game = false;
     storyContent.hidden = false;
     buts.hidden = false;
@@ -24,133 +21,100 @@ function continueStory(){
     showStory(index)
 }
 
-function showStory(textIndex){
-    while(continueBtn.firstChild){
-        continueBtn.removeChild(continueBtn.firstChild)
+function showStory(textIndex) {
+    while (continueBtn.firstChild) {
+        continueBtn.removeChild(continueBtn.firstChild) //Deletes all current buttons so the new ones can replace it
     }
-
-    console.log(textIndex)
-
-    if (!game){
-    const storyText = storyScript.find(storyText => storyText.id === textIndex)
-    storyElement.innerText = storyText.text
-
-    index = index + 1;
-
-        if(game){
-            game = false
-            console.log("tre")
-        }else{
+    if (!game) {
+        const storyText = storyScript.find(storyText => storyText.id === textIndex) //Sets the next story
+        storyElement.innerText = storyText.text
+        index = index + 1; //Makes sure we find the next story
+        if (game) {
+            game = false //Alternates between the story and the game
+        } else {
             game = true
-            console.log("fas")
         }
-    
-        storyText.options.forEach(option => {           
-        const button = document.createElement('button')
-        button.innerText = option.text
-        button.classList.add('btn')
-        
-        button.addEventListener('click', () => selection(option)) //This is the "continue" button event
-        continueBtn.appendChild(button)
-                   
+        storyText.options.forEach(option => { //Loops through each button we want to make
+            const button = document.createElement('button') //Creates a new button evertime
+            button.innerText = option.text //Sets the text to the current option defined
+            button.classList.add('btn') //Adds the correct class for CSS
+            button.addEventListener('click', () => selection(option)) //This is the "next step" button event
+            continueBtn.appendChild(button)
         })
-    }else{
+    } else {
         //Start game mode
         storyReturn = false;
-        console.log("game")
         wordScramPlay()
-
     }
 }
 
-function showOption(option)
-{
-    return option.requiredState == null || option.requiredState(state);
-}
-
-function selection(option){
+function selection(option) { //Setting the next option after selected option
     let next = option.nextText;
-    while(continueBtn.firstChild){
+    while (continueBtn.firstChild) {
         continueBtn.removeChild(continueBtn.firstChild)
     }
-    
-    console.log(option)
 
-    if (next <= 0){
+    if (next <= 0) {
         document.location.reload();
     }
 
-    if (option.correct){
+    if (option.correct) { //Checks if the answer is the correct one so we can add time or not
         next = 11;
-        const storyText = storyScript.find(storyText => storyText.id === next)
+        const storyText = storyScript.find(storyText => storyText.id === next) //Sets the "Correct text"
         storyElement.innerText = storyText.text
-
-
         const button = document.createElement('button')
         button.innerText = "Continue"
         button.classList.add('btn')
         time = 10;
         time += 7;
-        button.addEventListener('click', () => selectNext(option)) //This is the "continue" button event
+        button.addEventListener('click', () => selectNext(option))
         continueBtn.appendChild(button)
-        
-        console.log("Answer is correct")
-        
-    }
-    else{
+    } else {
         next = 12;
         const storyText = storyScript.find(storyText => storyText.id === next)
         storyElement.innerText = storyText.text
-
-
-    const button = document.createElement('button')
+        const button = document.createElement('button')
         button.innerText = "Continue"
         button.classList.add('btn')
         time = 10;
         button.addEventListener('click', () => selectNext(option))
         continueBtn.appendChild(button)
-        console.log("Answer is incorrect")
     }
-
 }
 
-function selectNext(option){
-    const next = option.nextText;
-    
-   //state = Object.assign(state, option.setState)
-
+function selectNext(option) {
+    const next = option.nextText; //Going next
     showStory(next)
 }
 
 var difficultyCookieExists;
-if (window.location.href.match('index.html') != null){
+if (window.location.href.match('index.html') != null) {
     difficultyCookieExists = getCookie('setDifficulty');
-    if (difficultyCookieExists == ""){ 
+    if (difficultyCookieExists == "") {
         console.log('No cookie found');
-    }
-    else{
+    } else {
         difficulty = difficultyCookieExists;
         console.log("game diffy to " + difficulty);
     }
 }
 
-function getCookie(cookieName) {
+function getCookie(cookieName) { //Getting cookie again (Same as in mainPageJavaScript)
     var name = cookieName + "=";
     var decodedCookie = decodeURIComponent(document.cookie);
     var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
     }
     return "";
-  }
+}
 
-const storyContent = document.querySelector(".content")
+const storyContent = document.querySelector(".content") //Setting all the elements for the game environment
 const buts = document.querySelector(".buttons")
 const area = document.querySelector('.gameArea');
 
@@ -165,67 +129,59 @@ var game1 = false;
 var newW = "";
 var randomV = "";
 
-//Easy words
-const words1 = ['Test','Bit','Bug','Code','File','Load','Loop','Node','Wifi','LED'];
+//Easy words to choose from
+const words1 = ['Test', 'Bit', 'Bug', 'Code', 'File', 'Load', 'Loop', 'Node', 'Wifi', 'LED'];
 //Medium words
-const words2 = ['Input','Array','ASCII','Binary','Clock','Cookie','Queue','Stack','Virus','Mouse'];
+const words2 = ['Input', 'Array', 'ASCII', 'Binary', 'Clock', 'Cookie', 'Queue', 'Stack', 'Virus', 'Mouse'];
 //Hard words
-const words3 = ['Address','Download','Databus','Internet','Wireless','Spyware','Software','Program'];
+const words3 = ['Address', 'Download', 'Databus', 'Internet', 'Wireless', 'Spyware', 'Software', 'Program'];
 
- const createWords = () => {
-    if (difficulty == 1){
-        let number = Math.floor(Math.random() * words1.length);
+const createWords = () => {
+    if (difficulty == 1) {
+        let number = Math.floor(Math.random() * words1.length); //Picking a random word from the pool depending on difficulty
         let random = words1[number];
         return random;
-    }else if (difficulty == 2){
+    } else if (difficulty == 2) {
         let number = Math.floor(Math.random() * words2.length);
         let random = words2[number];
         return random;
-    }else{
+    } else {
         let number = Math.floor(Math.random() * words3.length);
         let random = words3[number];
         return random;
     }
+}
 
-     
- }
-
- const jumbleWord = (jWord) => {
-     for (let i = jWord.length - 1; i >= 0; i--) {
-         let temp = jWord[i];
-         let j = Math.floor(Math.random() * (i + 1));
-         jWord[i] = jWord[j];
-         jWord[j] = temp;
-
-     }
-     return jWord.join("");
- } 
-  
-function choice(){
-    if (!storyReturn){
-        wordScramPlay();
-        console.log("Starting game")
+const jumbleWord = (jWord) => {
+    for (let i = jWord.length - 1; i >= 0; i--) { //Looping through each character in the word
+        let temp = jWord[i];
+        let j = Math.floor(Math.random() * (i + 1)); //Randomly changing them
+        jWord[i] = jWord[j]; //Assigning their new position
+        jWord[j] = temp;
     }
-    else{
-        clearTimeout(t);
+    return jWord.join("");
+}
+
+function choice() {
+    if (!storyReturn) { //If the user got the word correct or not
+        wordScramPlay();
+    } else {
+        clearTimeout(t); //If they got the word correct then it returns to the story
         continueStory();
     }
 }
- 
-function timer(){
+
+function timer() { //Timer for the game
     score.innerText = time.toString();
     time--;
-    t = setTimeout("timer()",1000);
-
-        if (time === -1){
-            clearInterval(t);
-            console.log("Fail")
-            gameFailed();
-        }
-    
+    t = setTimeout("timer()", 1000);
+    if (time === -1) { //Counting down until -1 so that the game will fail
+        clearInterval(t);
+        gameFailed();
+    }
 }
 
-function gameFailed(){
+function gameFailed() {
 
     story.innerText = "You have taken too long to unscramble the word. The guards find you and you are caught.";
     story.hidden = false;
@@ -236,25 +192,22 @@ function gameFailed(){
     const button = document.createElement('button')
     button.innerText = "Restart game"
     button.classList.add('btn')
-    
+
     button.addEventListener('click', () => document.location.reload())
     continueBtn.appendChild(button)
 }
 
 const log = document.getElementById('log');
-        document.addEventListener('keypress', key);
-        function key(e) {
-          if (`${e.code}` == "Enter"){
-              if(game1){
-                console.log("eeeee word");
-                choice();
-              }
-          }
+document.addEventListener('keypress', key); //Listens for the keypress of Enter to make it quick for the user to submit their word
+function key(e) {
+    if (`${e.code}` == "Enter") {
+        if (game1) {
+            choice();
         }
+    }
+}
 
-
-
- function wordScramPlay() {
+function wordScramPlay() {
 
     //Showing game
     storyContent.hidden = true;
@@ -263,33 +216,21 @@ const log = document.getElementById('log');
     guess.hidden = false;
     guess.focus();
 
-     if (!game1) {
-
+    if (!game1) { //If we are just going into the game for the first time
         game1 = true;
         inital.style.color = 'black';
         gameBut.textContent = 'Guess';
         guess.classList.toggle('hidden');
-        
-        newW = createWords();
+        newW = createWords(); //Creates a new word to be unscrambled
         randomV = jumbleWord(newW.split(""));
-        console.log(randomV);
         inital.innerHTML = "Unscramble the word: " + randomV;
-        timer();
+        timer(); //Starts the timer
         storyReturn = false;
-
-     } else {
-
-        
-
-        let inputWord = guess.value.toLowerCase();
-         if (inputWord == newW.toLowerCase()) {
-            console.log("Input: " + guess.value);
-            console.log("Scram: " + newW);
-            console.log("correct word");
-
+    } else { //If this is the second attempt
+        let inputWord = guess.value.toLowerCase(); //Converts to lower case so that it is easy to check input
+        if (inputWord == newW.toLowerCase()) {
             guess.hidden = true;
-            clearTimeout(t);
-            console.log('correct');
+            clearTimeout(t); //Restarts the timer
             game1 = false;
             inital.innerHTML = `Well done! That's correct.\n
             The word was ${newW}`;
@@ -297,84 +238,78 @@ const log = document.getElementById('log');
             guess.classList.toggle('hidden');
             gameBut.innerHTML = 'Continue story';
             guess.value = "";
-            storyReturn = true;
-
-         } else {
-            console.log('incorrect');
+            storyReturn = true; //Returns to the story
+        } else {
             inital.innerHTML = `Incorrect guess.Try Again before time runs out! '${randomV}'`;
             inital.style.color = 'red';
             gameBut.innerHTML = 'Guess';
             guess.value = "";
             guess.classList.toggle('hidden');
-            storyReturn = false;
-         }
-     }
- }
+            storyReturn = false; //Can retry the guess
+        }
+    }
+}
 
-const storyScript = [
+const storyScript = [ //All of the story options in their objects so that they can be accessed easily
     {
-        id: 1, 
-        text: 'Crossing the border with your backpack is easy and you are able to'+
-        ' hitchhike your way to the nearby town at the bottom of the mountain. '+
-        'From there you steal a car and start to drive up to the first, yet weak, '+
-        'checkpoint at the base on the mountain where you choose between your laptop or your '
-        +'phone to hack into the registration plate recognition cameras to allow your car through.', 
-        options:[
-            { text: "Laptop",
-            correct: true,
-            nextText: 2
+        id: 1,
+        text: 'Crossing the border with your backpack is easy and you are able to' +
+            ' hitchhike your way to the nearby town at the bottom of the mountain. ' +
+            'From there you steal a car and start to drive up to the first, yet weak, ' +
+            'checkpoint at the base on the mountain where you choose between your laptop or your ' +
+            'phone to hack into the registration plate recognition cameras to allow your car through.',
+        options: [{
+                text: "Laptop",
+                correct: true,
+                nextText: 2
             },
-            { text: "Phone", 
-            correct: false,
-            nextText: 2  
+            {
+                text: "Phone",
+                correct: false,
+                nextText: 2
             }
         ]
     },
     {
         id: 2,
-        
+
         text: 'Once you are through you ditch the car halfway up and continue on foot where you are met with the main gate to the compound with guards checking papers outside. You decide the best idea is to distract the guards by hijacking the flood lights and choose between a pair of plyers or some extra wires. ',
-        options: [
-            { 
+        options: [{
                 text: "Plyers",
                 correct: false,
                 nextText: 3
             },
-            { 
+            {
                 text: "Spare wires",
                 correct: true,
                 nextText: 3
             }
         ]
     },
-    
     {
-        id: 3, 
+        id: 3,
         text: 'You make your way to the gate controls and decide whether to use the plyers again or a screwdriver. ',
-        options: [
-            { 
+        options: [{
                 text: "Plyers",
                 correct: true,
                 nextText: 4
             },
-            { 
+            {
                 text: "Screw driver",
                 correct: false,
                 nextText: 4
             }
         ]
     },
-
     {
-        id: 4, 
+        id: 4,
         text: 'As you move through the car park you decide to set off some car alarms to distract the guards from the building, you choose between a rock and your phone.',
-        options: [
-            { 
+        options: [{
                 text: "Rock",
                 correct: false,
                 nextText: 5
             },
-            { 
+            {
                 text: "Phone",
                 correct: true,
                 nextText: 5
@@ -382,15 +317,14 @@ const storyScript = [
         ]
     },
     {
-        id: 5, 
+        id: 5,
         text: 'You get to the back entrance where there is a pattern recognition panel. You decide to use either yesterday’s newspaper and your phone or a screwdriver.',
-        options: [
-            { 
+        options: [{
                 text: "Newspaper and phone",
                 correct: true,
                 nextText: 5
             },
-            { 
+            {
                 text: "Screw driver",
                 correct: false,
                 nextText: 5
@@ -398,15 +332,14 @@ const storyScript = [
         ]
     },
     {
-        id: 6, 
+        id: 6,
         text: 'After you make it inside you need to get past the guards from the server room door. You decide to make a fake radio call using either your phone or laptop.',
-        options: [
-            { 
+        options: [{
                 text: "Phone",
                 correct: true,
                 nextText: 5
             },
-            { 
+            {
                 text: "Laptop",
                 correct: false,
                 nextText: 5
@@ -414,15 +347,14 @@ const storyScript = [
         ]
     },
     {
-        id: 7, 
+        id: 7,
         text: 'Now you need to get through the server room door fingerprint scanner choosing between a special silicon glove with 5 options or try all 10 of your own fingers.',
-        options: [
-            { 
+        options: [{
                 text: "Glove",
                 correct: true,
                 nextText: 5
             },
-            { 
+            {
                 text: "Fingers",
                 correct: false,
                 nextText: 5
@@ -430,15 +362,14 @@ const storyScript = [
         ]
     },
     {
-        id: 8, 
+        id: 8,
         text: 'Once inside you need to get through the main server. You must break through every layer of defence you have come across so far. Choose between the computer that is already inside the room or the laptop from your bag.',
-        options: [
-            { 
+        options: [{
                 text: "Computer",
                 correct: true,
                 nextText: 5
             },
-            { 
+            {
                 text: "Your laptop",
                 correct: false,
                 nextText: 5
@@ -446,15 +377,14 @@ const storyScript = [
         ]
     },
     {
-        id: 9, 
+        id: 9,
         text: 'Now that you have successfully deleted your data file you must escape. Setting off the fire alarm system to cause panic inside the building is the safest way to escape, will you start a real fire or manually access the system through the computer in the server room?',
-        options: [
-            { 
+        options: [{
                 text: "Start fire",
                 correct: false,
                 nextText: 5
             },
-            { 
+            {
                 text: "Manually trigger alarm",
                 correct: true,
                 nextText: 5
@@ -462,22 +392,20 @@ const storyScript = [
         ]
     },
     {
-        id: 10, 
+        id: 10,
         text: 'MISSION COMPLETE: You sneak your way out the back entrance as panic ensues leaving no trail behind you. After blending into the group of people outside you are able to leave the compound in hidden in the back of an employee’s car. Deleting your file was the first major step in being able to live in this new nation and complete the rest of your mission.',
-        options: [
-            { 
-                text: "Complete Mission",
-                
-                nextText: -1
-            }
-        ]
+        options: [{
+            text: "Complete Mission",
+
+            nextText: -1
+        }]
     },
     {
-        id: 11, 
+        id: 11,
         text: 'Well done, you chose the correct option. You have an additional 8 seconds to complete the game.',
     },
     {
-        id: 12, 
+        id: 12,
         text: 'You did not choose the best option for speed and effectivness. Your time limit will remain the same.',
     }
 ]
--- mainPageJavaScript.js
+++ mainPageJavaScript.js
@@ -1,110 +1,87 @@
 const menuButton = document.querySelector('.menu');
 const show = document.querySelector('.select');
-let menuOpen = false;
+var menuOpen = false;
 
-
-menuButton.addEventListener('click',() =>{
-    if(!menuOpen){
-        menuButton.classList.add('open');
+menuButton.addEventListener('click', () => {
+    if (!menuOpen) {
+        menuButton.classList.add('open'); //Adds to class list so that we know what stage (on/off)
         show.classList.add('fade');
         menuOpen = true;
-        
-    }
-    else{
+    } else {
         menuButton.classList.remove('open');
         show.classList.remove('fade');
         menuOpen = false;
-        
     }
 });
-
 
 var playTypewriter;
 var soundCookieExists;
 var difficultyCookieExists;
 
-if (window.location.href.match('settingsPage.html') != null){
+if (window.location.href.match('settingsPage.html') != null) { //If a specific page is loaded
     //Checks the cookie for the sounds setting
     soundCookieExists = getCookie('playTypewriter');
     difficultyCookieExists = getCookie('setDifficulty');
 
-    if (soundCookieExists == ""){ 
+    if (soundCookieExists == "") { //Checking if the cookie exsists and sets a default value
         console.log('No cookie found');
-
         playTypewriter = "0";
-    }
-    else{
-        if(soundCookieExists == "1"){
+    } else {
+        if (soundCookieExists == "1") { //Different options depending on what was saved
             document.getElementById('soundCheckbox').checked = true;
             playTypewriter = soundCookieExists;
-            console.log("Setting to checked");
-        }
-        else{
+        } else {
             document.getElementById('soundCheckbox').checked = false;
             playTypewriter = "0";
-            console.log("Setting to unchecked");
         }
     }
 
-
-    if (difficultyCookieExists == ""){ 
+    if (difficultyCookieExists == "") { //Error if nothing
         console.log('No cookie found');
-    }
-    else{
-        document.getElementById('difficulty').value = difficultyCookieExists;
-        console.log("sa difficulty to " + difficultyCookieExists);
+        difficultyCookieExists = 1;
+    } else {
+        document.getElementById('difficulty').value = difficultyCookieExists; //Sets cookie to the selected value
     }
 }
 
-
-function setTypewriter(){
+function setTypewriter() { //Ff the cookie is found and correct
     const soundCheck = document.getElementById("soundCheckbox");
-    if(soundCheck.checked){
+    if (soundCheck.checked) {
         playTypewriter = "1"
-        console.log("Setting cookie to checked");
         document.cookie = "playTypewriter=" + playTypewriter + "; expires=Thu, 18 Dec 2060 12:00:00 UTC;";
-    }else{
+    } else {
         playTypewriter = "0";
-        console.log("Setting cookie to unchecked");
         document.cookie = "playTypewriter=" + playTypewriter + "; expires=Thu, 18 Dec 2060 12:00:00 UTC;";
-
     }
-    
 }
 
 var setDifficulty;
-function setDifficulty(){
+
+function setDifficulty() { //Setting difficulty depending on the cookie
     const difficulty = document.getElementById("difficulty");
-
-
-    console.log("setDifficulty " + difficulty.value);
-     document.cookie = "setDifficulty=" + difficulty.value + "; expires=Thu, 28 Dec 2060 12:00:00 UTC;";
+    document.cookie = "setDifficulty=" + difficulty.value + "; expires=Thu, 28 Dec 2060 12:00:00 UTC;";
 }
+
 var typewriter = document.getElementById('typewriter');
 
-function playType(){
-    
+function playType() { //Plays a typewriter click sound if cookie is correct
     var soundCookie = getCookie('playTypewriter');
-        if(soundCookie == "1"){
-            console.log("Play typewriter")
-            typewriter.play();
-        }
+    if (soundCookie == "1") {
+        typewriter.play();
+    }
 }
 
 var play = 0;
 var music = document.getElementById('audio');
 var muteUnmuteBtn = document.getElementById('audioMUMBtn')
-function playMusic(){
 
-    if (play === "1"){
+function playMusic() { //Plays the music when the image is clicked
+    if (play === "1") {
         music.pause();
-        console.log("Not playing");
         play = "0";
         muteUnmuteBtn.src = "Images/volume-mute-solid.svg";
-    }
-    else{
+    } else {
         music.play();
-        console.log("Playing");
         play = "1";
         muteUnmuteBtn.src = "Images/volume-up-solid.svg";
     }
@@ -112,20 +89,20 @@ function playMusic(){
 
 //Gets any cookie from the given name
 function getCookie(cookieName) {
-    var name = cookieName + "=";
+    var name = cookieName + "="; //Gets the cookie name
     var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
+    var ca = decodedCookie.split(';'); //Delimiter
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i]; //Loops through each section from the delimiter
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length); //Sets the cookie to correct value
+        }
     }
-    return "";
-  }
+    return ""; //If it doesn't exist then send back nothing
+}
 
-  /*getCookie function taken from W3 Schools as it is a very 
-  clear way to get data from any cookie*/
+/*getCookie function taken from W3 Schools as it is a very 
+clear way to get data from any cookie*/
